d591044df92d70c96563c024e94e3e33d708fe8a
Sofia G <44249466+cctacog@users.noreply.github.com>
2020-07-16 17:02:19 -0700

    Create txtadv.hpp

---
+++ txtadv.hpp
@@ -0,0 +1,37 @@
+#include <vector>
+using namespace std;
+
+int main();
+void choice1(int stay_leave, int tries, string first, string last);
+void choice2(char a_c, int tries, string first, string last);
+void choice3(string n_q, int tries, string first, string last);
+void choice4(string i_g, int tries, string first, string last);
+void choice5(int p_w, int tries, string first, string last);
+void choice6(string m_s, int tries, string first, string last);
+void choice7(int q_s, int p_w, int tries, string first, string last);
+void choice8(int t_l, string nfirst, string nlast, int tries, string first, string last);
+void choice9(char choice, string nfirst, string nlast, int tries, string first, string last);
+void beginning(string first, string last, int tries);
+void stay(int tries, string first, string last);
+void leave(int tries, string first, string last);
+void g_fire(int tries, string first, string last);
+void pulled_away(int tries, string first, string last);
+void pulled_away2(int tries, string first, string last);
+void side(int tries, string first, string last);
+void following(int tries, string first, string last);
+void service(int tries, string first, string last);
+void quiet(int tries, string first, string last);
+void still(int p_w, int tries, string first, string last);
+void slapped(int tries, string first, string last);
+void untied(int tries, string first, string last);
+void yrs_later(string nfirst, string nlast, int tries, string first, string last);
+int question(int tries, string first, string last);
+int restart(char y_n, int tries, string first, string last);
+void ending1(int tries, string first, string last);
+void ending2(int tries, string first, string last);
+void ending3(int tries, string first, string last);
+void ending4(int tries, string first, string last);
+void ending5(string nfirst, string nlast, int tries, string first, string last);
+void ending6(int tries, string first, string last);
+void ending7(int tries, string first, string last);
+
